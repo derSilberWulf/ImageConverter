1c9df189c45a35cf3586012ee912a88d7f8b717c
unknown <yahna.3@wright.edu>
2016-05-06 15:07:26 -0400

    Added a view for downloading files

--- server/imageconverter/converter/views.py
+++ server/imageconverter/converter/views.py
@@ -4,9 +4,30 @@ from .models import StoredImage
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import SessionAuthentication
+from rest_framework import generics
 
 from .serializers import StoredImageSerializer
 from .permissions import IsOwner
+
+
+import os
+from django.http import HttpResponse
+from django.core.servers.basehttp import FileWrapper
+#TO DO: Fix error messages when objects don't exist
+class DownloadView(generics.GenericAPIView):
+    permission_classes = (IsAuthenticated, IsOwner, )
+    authentication_classes = (SessionAuthentication,)
+    def get(self, request, pk):
+        """
+        Send back a file.
+        """
+        filename = StoredImage.objects.filter(id=pk, owner=request.user)[0].image.path
+        wrapper = FileWrapper(StoredImage.objects.filter(id=9, owner=request.user)[0].image.file)
+        response = HttpResponse(wrapper, content_type='image')
+        response['Content-Length'] = os.path.getsize(filename)
+        #replace attachment with inline to get browser to display image
+        response['Content-Disposition'] = 'attachment; filename=' + os.path.basename(filename)
+        return response
 
 
 class ImageViewSet(viewsets.ModelViewSet):
